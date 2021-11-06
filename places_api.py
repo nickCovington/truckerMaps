55a7505f7e2ccdbefdc5f8ee1a37618cf29f7844
Ubuntu <ncovington3@garlic.o2ckh3uuju2ebit0f2va4on4tc.bx.internal.cloudapp.net>
2021-11-06 23:30:39 +0000

    places api is working

--- places_api.py
+++ places_api.py
@@ -1,10 +1,28 @@
 import requests
 
-url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=123%20main%20street&key=YOUR_API_KEY"
+# url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=4825%20dunwoody%20station%20drive&key=ENTER_API_KEY"
 
-payload = {}
-headers = {}
+# payload = {}
+# headers = {}
 
-response = requests.request("GET", url, headers=headers, data=payload)
+# response = requests.request("GET", url, headers=headers, data=payload)
 
-print(response.text)
+# print(response.text)
+
+
+def getPlaceInfo(placeName):
+    url = (
+        "https://maps.googleapis.com/maps/api/place/textsearch/json?query="
+        + placeName
+        + "&key=ENTER_API_KEY"
+    )
+
+    payload = {}
+    headers = {}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    print(response.text)
+
+
+getPlaceInfo("4815 dunwoody station drive")
