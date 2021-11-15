8f298163bc25014f538bf7993d2e20aea8468fab
Kofi-connection <kosae1@student.gsu.edu>
2021-11-15 07:03:04 -0500

    added delivery details

--- app.py
+++ app.py
@@ -1,71 +1,318 @@
+from enum import unique
 import os
 import json
 import flask
+import time
 from flask_login import login_user, current_user, LoginManager
 from flask_login.utils import login_required
+from flask.templating import render_template
+from flask_wtf.recaptcha import validators
+from werkzeug.utils import redirect
+from wtforms.fields.simple import SubmitField
+from flask_sqlalchemy import SQLAlchemy
+import flask_login
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
+from flask_wtf import FlaskForm
+from wtforms import RadioField
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Length, ValidationError
+from places_api import getPlaceInfo
 
 import random
 import base64
 import requests
 
 app = flask.Flask(__name__)
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
+app.config["SECRET_KEY"] = "testing"
 
-@app.route('/index')
-@login_required
-def index():
-    return flask.render_template("index.html")
+db = SQLAlchemy(app)
+
+# app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL").replace(
+#     "://", "ql://", 1
+# )
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 login_manager = LoginManager()
-login_manager.login_view = "login"
 login_manager.init_app(app)
+login_manager.login_view = "login"
 
 
 @login_manager.user_loader
-def load_user(user_name):
-    #return the user_name from the database and load the user
-    return 
-@app.route('/signup')
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(120), nullable=False)
+    usrType = db.Column(db.String(8))
+    email = db.Column(db.String(120))
+    fName = db.Column(db.String(120))
+
+
+class warehouse(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userID = db.Column(db.Integer)
+    name = db.Column(db.String(120))
+    lat = db.Column(db.Float)
+    lng = db.Column(db.Float)
+    address = db.Column(db.String(240))
+
+
+class delivery(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    expectedAt = db.Column(db.String(120))
+    deliveredAt = db.Column(db.String(120))
+    userID = db.Column(db.Integer, unique=True)
+    warehouseID = db.Column(db.Integer, unique=True)
+    details = db.Column(db.String(240))
+
+
+# Registration validation
+class RegisterForm(FlaskForm):
+    username = StringField(
+        validators=[InputRequired(), Length(min=2, max=63)],
+        render_kw={"placeholder": "Username"},
+    )
+    usrType = StringField(
+        validators=[InputRequired(), Length(min=7, max=8)],
+        render_kw={"placeholder": "User Type - Manager or Trucker"},
+    )
+
+    password = PasswordField(
+        validators=[InputRequired(), Length(min=2, max=63)],
+        render_kw={"placeholder": "Password"},
+    )
+    boolchoice = RadioField('Label', choices=[('manager','Manager'),('trucker','Trucker')])
+
+    submit = SubmitField("SignUp")
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(username=username.data).first()
+
+        if existing_user_username:
+            raise ValidationError("Username already exists, choose a different one")
+
+
+class LoginForm(FlaskForm):
+    username = StringField(
+        validators=[InputRequired(), Length(min=2, max=63)],
+        render_kw={"placeholder": "Username"},
+    )
+
+    password = PasswordField(
+        validators=[InputRequired(), Length(min=2, max=63)],
+        render_kw={"placeholder": "Password"},
+    )
+
+    submit = SubmitField("Login")
+
+
+class UpdateForm(FlaskForm):
+    username = StringField(
+        validators=[InputRequired(), Length(min=0, max=63)],
+        render_kw={"placeholder": "Username"},
+    )
+    usrType = StringField(
+        validators=[InputRequired(), Length(min=0, max=8)],
+        render_kw={"placeholder": "Change user type - Manager or Trucker"},
+    )
+    email = StringField(
+        validators=[InputRequired(), Length(min=0, max=63)],
+        render_kw={"placeholder": "Enter new email"},
+    )
+    fName = StringField(
+        validators=[InputRequired(), Length(min=0, max=63)],
+        render_kw={"placeholder": "Update your full name"},
+    )
+
+    submit = SubmitField("Update Profile")
+
+
+db.create_all()
+
+
+def get_profile_details():
+    id = flask_login.current_user.id
+    usrType = flask_login.current_user.usrType
+    userName = flask_login.current_user.username
+    try:
+        email = flask_login.current_user.email
+        fName = flask_login.current_user.fName
+    except:
+        email = "No email given"
+        fName = flask_login.current_user.fName
+    profile_details = {
+        "id": id,
+        "usrType": usrType,
+        "userName": userName,
+        "email": email,
+        "fName": fName,
+    }
+    return profile_details
+
+
+@app.route("/home", methods=["GET", "POST"])
+@login_required
+def home():
+    userID = flask_login.current_user.id
+    # if user attempts to add new location
+    if flask.request.method == "POST":
+        locationToAdd = flask.request.form.get("location-to-add")
+
+        # placeInfo = [address, latitude, longitude]
+        placeInfo = getPlaceInfo(locationToAdd)
+
+        # TO-DO: need to actually add (address), (latitude), & (longitude) into DB
+        address = placeInfo["address"]
+        latitude = placeInfo["lat"]
+        longitude = placeInfo["lon"]
+        name = placeInfo["name"]
+        
+
+        wexists = warehouse.query.filter_by(address=address, userID=userID).first()
+        if wexists:
+            flask.flash(address + " already exists in your saved list")
+        else:
+            new_w = warehouse(
+                name=name,
+                address=address,
+                lat=latitude,
+                lng=longitude,
+                userID=userID
+            )
+            db.session.add(new_w)
+            db.session.commit()
+
+            flask.flash(placeInfo["address"] + " has been successfully added.")
+    # The user adds the delivery information
+    
+    w_list = warehouse.query.filter_by(userID=userID).all()
+    existing_adds = []
+    for wh in w_list:
+        existing_adds.append(wh.address)
+
+    return flask.render_template(
+        "home.html",
+        usern=flask_login.current_user.username,
+        warehouses=existing_adds,
+        len=len(existing_adds),
+        deliveryDetails=deliveryTable()
+    )
+def deliveryTable():
+    timeExpected= flask.request.form.get("expected_at")
+    localTime=time.ctime(time.time())
+    timeDelivered=flask.request.form.get("timeDelivered")
+    details=flask.request.form.get("details")
+    deliveryInfo=delivery(
+        expectedAt=timeExpected,
+        deliveredAt=timeDelivered,
+        details=details,
+
+    )
+    db.session.add(deliveryInfo)
+    db.session.commit()
+    deliveryDetails={
+        "expected_at":timeExpected,
+        "timeDelivered":timeDelivered,
+        "details":details,
+    }
+
+    return deliveryDetails
+    
+
+@app.route("/profile", methods=["GET", "POST"])
+@login_required
+def profile():
+    return flask.render_template("profile.html", data=get_profile_details())
+
+
+@app.route("/edit", methods=["GET", "POST"])
+@login_required
+def edit():
+    form = UpdateForm()
+    if flask.request.method == "POST":
+        if form.validate_on_submit():
+            curr = User.query.filter_by(
+                username=flask_login.current_user.username
+            ).first()
+            if form.fName.data != None:
+                curr.fName = form.fName.data
+            if form.username.data != None:
+                curr.usernme = form.username.data
+            if form.email.data != None:
+                curr.email = form.email.data
+            if form.usrType.data != None:
+                curr.usrType = form.usrType.data
+            db.session.commit()
+            return flask.redirect(flask.url_for("profile"))
+    return flask.render_template("edit.html", form=form, data=get_profile_details())
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return flask.redirect(flask.url_for("login"))
+
+
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
-    return flask.render_template("signup.html")
+    # check if the user from the form is in the database.
+    form = RegisterForm()
 
-@app.route("/signup", methods=["POST"])
-def signup_post():
-    username= flask.request.form.get('username')
-    #check if the user from the form is in the database.
-    """""
-    if user:
-        pass
-    else:
-        user = User(username=username)
-        db.session.add(user)
-        db.session.commit()
-    """""
-@app.route("/login")
+    if flask.request.method == "POST":
+        if form.validate_on_submit():
+            new_user = User(
+                username=form.username.data,
+                usrType=form.usrType.data,
+                password=form.password.data,
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            return flask.redirect(flask.url_for("login"))
+        else:
+            flask.flash("Username already exists")
+
+    return flask.render_template("signup.html", form=form)
+
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return flask.render_template("login.html")
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if form.password.data == user.password:
+                login_user(user)
+                return flask.redirect(flask.url_for("home"))
+            else:
+                flask.flash("Invalid password")
+        else:
+            return flask.redirect(flask.url_for("invalid"))
 
-@app.route("/login", methods=["POST"])
-def login_post():
-    username=flask.request.form.get("username")
-    #query database for the username in the form
-    user=username
-    if user:
-        login_user(user)
-        return flask.redirect(flask.url_for("index"))
-    else:
-        return flask.jsonify({"status": 401, "reason": "Username or Password Error"})
-@app.route('/save', methods=["POST"])
+    return flask.render_template("login.html", form=form)
+
+
+@app.route("/invalid")
+def invalid():
+    return flask.render_template("invalid.html")
+
+
+@app.route("/save", methods=["POST"])
 def save():
-    #add the warehouse location, name, and deliveries.
-    return flask.redirect(flask.url_for("index"))
+    # add the warehouse location, name, and deliveries.
+    return flask.redirect(flask.url_for("home"))
 
-@app.route('/')
+
+@app.route("/")
 def main():
     if current_user.is_authenticated:
-        return flask.redirect(flask.url_for("index"))
+        return flask.redirect(flask.url_for("home"))
     return flask.redirect(flask.url_for("login"))
-app.run(
-    host=os.getenv('IP','0.0.0.0'),
-    port=int(os.getenv('PORT',8080)),
-    debug=False
-)
+
+
+app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8080)), debug=False)
