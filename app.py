a44193639c2a71d081d3d969c3455a5eb2b3e310
Sai Tippana <saiT1108>
2021-11-16 22:31:43 -0500

    changed db link

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ db = SQLAlchemy(app)
 
 # app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL").replace(
 #     "://", "ql://", 1
-)
+#)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
